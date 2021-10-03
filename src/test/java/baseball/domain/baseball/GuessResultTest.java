ee294c1d6083d4b2313d1a362ee1a584398a59eb
YONGJU <odetsuo@gmail.com>
2021-10-03 14:57:27 +0900

    test : 추측 결과 생성 예외

--- src/test/java/baseball/domain/baseball/GuessResultTest.java
+++ src/test/java/baseball/domain/baseball/GuessResultTest.java
@@ -1,10 +1,12 @@
 package baseball.domain.baseball;
 
+import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class GuessResultTest {
 
@@ -26,8 +28,8 @@ class GuessResultTest {
     @DisplayName("스트라이크")
     void strike() {
         //given
-        GuessResult guessResult = new GuessResult();
-        guessResult = guessResult.addResult(GuessState.STRIKE);
+        GuessResult guessResult = new GuessResult()
+                .addResult(GuessState.STRIKE);
 
         //when
         int actual = guessResult.strike();
@@ -41,8 +43,8 @@ class GuessResultTest {
     @DisplayName("볼")
     void ball() {
         //given
-        GuessResult guessResult = new GuessResult();
-        guessResult = guessResult.addResult(GuessState.BALL);
+        GuessResult guessResult = new GuessResult()
+                .addResult(GuessState.BALL);
 
         //when
         int actual = guessResult.ball();
@@ -56,9 +58,9 @@ class GuessResultTest {
     @DisplayName("스트라이크와 볼")
     void strike_ball() {
         //given
-        GuessResult guessResult = new GuessResult();
-        guessResult = guessResult.addResult(GuessState.STRIKE);
-        guessResult = guessResult.addResult(GuessState.BALL);
+        GuessResult guessResult = new GuessResult()
+                .addResult(GuessState.STRIKE)
+                .addResult(GuessState.BALL);
 
         //when
         int actualStrike = guessResult.strike();
@@ -71,6 +73,25 @@ class GuessResultTest {
                 () -> assertThat(actualBall).isEqualTo(1),
                 () -> assertThat(actualNothing).isFalse()
         );
+
+    }
+
+    @Test
+    @DisplayName("결과 생성 예외")
+    void exception() {
+        //given
+        GuessResult guessResult = new GuessResult()
+                .addResult(GuessState.STRIKE)
+                .addResult(GuessState.STRIKE)
+                .addResult(GuessState.STRIKE);
+
+
+        //when
+        ThrowableAssert.ThrowingCallable actual = () -> guessResult.addResult(GuessState.STRIKE);
+
+        //then
+        assertThatThrownBy(actual).isInstanceOf(GuessResultException.class)
+                .hasMessage("스트라이크 또는 볼은 0~3 사이의 숫자만 가능합니다. 현재 값 : 4");
 
     }
 
