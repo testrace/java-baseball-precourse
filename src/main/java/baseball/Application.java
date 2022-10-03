7a035d1ee973a755e31004cda10646babfd58cb6
YONGJU <odetsuo@gmail.com>
2022-10-03 17:55:44 +0900

    feat: 입력한 숫자와 컴퓨터의 숫자가 일치 결과를 계산하여 출력
    게임이 종료되면 다시 시작할 수 있다.
    예외가 발생하면 프로그램은 종료된다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,43 @@
 package baseball;
 
+import baseball.application.BaseBallGame;
+import baseball.application.NumberPicker;
+import baseball.application.NumberSelector;
+import baseball.application.RandomNumberPicker;
+import baseball.application.dto.GuessResult;
+import baseball.ui.InputView;
+import baseball.ui.ResultView;
+import java.util.List;
+
 public class Application {
+
+    private static final int COUNT_OF_NUMBERS = 3;
+    private static final NumberSelector NUMBER_SELECTOR = new NumberSelector();
+    private static final NumberPicker NUMBER_PICKER = new RandomNumberPicker();
+    private static final BaseBallGame BASE_BALL_GAME = new BaseBallGame();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        play(NUMBER_SELECTOR.select(COUNT_OF_NUMBERS, NUMBER_PICKER));
+
+        while (InputView.continueGames()) {
+            play(NUMBER_SELECTOR.select(COUNT_OF_NUMBERS, NUMBER_PICKER));
+        }
+    }
+
+    private static void play(final List<Integer> selectedNumbers) {
+        tryGuess(selectedNumbers);
+        ResultView.printGameOver();
+    }
+
+    private static void tryGuess(List<Integer> selectedNumbers) {
+        GuessResult guessResult = new GuessResult();
+
+        while (!guessResult.isGameOver()) {
+            String inputNumbers = InputView.inputNumbers();
+            guessResult = BASE_BALL_GAME.guess(selectedNumbers, inputNumbers);
+
+            ResultView.printScore(guessResult);
+        }
     }
 }
---
+++ src/main/java/baseball/application/BaseBallGame.java
@@ -0,0 +1,36 @@
+package baseball.application;
+
+import baseball.application.dto.GuessResult;
+import baseball.domain.GuessScore;
+import baseball.domain.GuessState;
+import baseball.domain.number.LocationNumber;
+import baseball.domain.number.Numbers;
+import baseball.domain.number.NumbersFactory;
+import java.util.List;
+
+public class BaseBallGame {
+
+    public GuessResult guess(List<Integer> selectedNumbers, String inputNumbers) {
+        Numbers hostNumbers = NumbersFactory.convert(selectedNumbers);
+        Numbers clientNumbers = NumbersFactory.convert(inputNumbers);
+        GuessScore guessScore = new GuessScore();
+        for (LocationNumber clientNumber : clientNumbers.getLocationNumbers()) {
+            guessScore.addScore(getGuessState(hostNumbers, clientNumber));
+        }
+
+        return createGuessResult(guessScore);
+    }
+
+    private static GuessResult createGuessResult(GuessScore guessScore) {
+        GuessResult guessResult = new GuessResult();
+        guessResult.setBall(guessScore.getScore(GuessState.BALL));
+        guessResult.setStrike(guessScore.getScore(GuessState.STRIKE));
+        guessResult.setGameOver(guessScore.isGameOver());
+        return guessResult;
+    }
+
+    private GuessState getGuessState(Numbers hostNumbers, LocationNumber clientNumber) {
+        return GuessState.of(hostNumbers, clientNumber);
+    }
+
+}
